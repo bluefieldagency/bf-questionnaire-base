3a8187a1789d5510704a6c490d7e490b5590707e
Erik <erik@holdorp.nl>
2022-05-17 12:28:36 +0200

    Config file publishable?

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -30,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
 //        Relation::morphMap([
 //            'MorphOrder' => Order::class,
 //        ]);
+
+        $this->registerPublishing();
 	}
 
 	/**
