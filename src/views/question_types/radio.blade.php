ed07c13bc7c9dd938528c7107578f30baf63892a
Erik <erik@holdorp.nl>
2022-05-19 21:10:23 +0200

    required attribute

--- src/views/question_types/radio.blade.php
+++ src/views/question_types/radio.blade.php
@@ -1,7 +1,15 @@
 <div class="question-container">
     @foreach($question->answers as $answer)
         <div class="answer-container styled-radio">
-            <input type="radio" id="question_{{ $question->id }}_answer_{{ $answer->id }}" name="question_{{ $question->id }}_answer" value="{{ $answer->id }}">
+            <input
+                type="radio"
+                id="question_{{ $question->id }}_answer_{{ $answer->id }}"
+                name="question_{{ $question->id }}_answer"
+                value="{{ $answer->id }}"
+                @if ($question->is_required)
+                    required
+                @endif
+            >
             <label for="question_{{ $question->id }}_answer_{{ $answer->id }}">{{ $answer->title }}</label>
         </div>
     @endforeach
--- src/views/question_types/text.blade.php
+++ src/views/question_types/text.blade.php
@@ -1,4 +1,12 @@
 <div class="question-container">
     <label for="question_{{ $question->id }}_answer">{{ $question->title }}</label>
-    <input type="text" id="question_{{ $question->id }}_answer" name="question_{{ $question->id }}_answer" placeholder="{{ optional($question->options)['placeholder'] }}">
+    <input
+        type="text"
+        id="question_{{ $question->id }}_answer"
+        name="question_{{ $question->id }}_answer"
+        placeholder="{{ optional($question->options)['placeholder'] }}"
+        @if ($question->is_required)
+            required
+        @endif
+    >
 </div>
