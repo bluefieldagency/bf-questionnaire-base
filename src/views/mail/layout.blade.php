59cae54f435782d6674dfaf65e9d505a7c38a07e
Erik <erik@holdorp.nl>
2022-06-14 00:40:53 +0200

    Mail styling

--- src/views/mail/layout.blade.php
+++ src/views/mail/layout.blade.php
@@ -16,84 +16,8 @@
     <!--[if !mso]><!-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
     <!--<![endif]-->
-    <style>
-        *,
-        *:after,
-        *:before {
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-        }
-        * {
-            -ms-text-size-adjust: 100%;
-            -webkit-text-size-adjust: 100%;
-        }
-        html,
-        body,
-        .document {
-            width: 100% !important;
-            height: 100% !important;
-            margin: 0;
-            padding: 0;
-        }
-        body {
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-            text-rendering: optimizeLegibility;
-            background: #ffffff;
-            font-family: Roboto, sans-serif !important;
-        }
-        div[style*="margin: 16px 0"] {
-            margin: 0 !important;
-        }
-        table,
-        td {
-            mso-table-lspace: 0pt;
-            mso-table-rspace: 0pt;
-        }
-        td, p {
-            font-family: Roboto, sans-serif !important;
-        }
-        table {
-            border-spacing: 0;
-            border-collapse: collapse;
-            table-layout: fixed;
-            margin: 0 auto;
-        }
-        img {
-            -ms-interpolation-mode: bicubic;
-            max-width: 100%;
-            border: 0;
-        }
-        *[x-apple-data-detectors] {
-            color: inherit !important;
-            text-decoration: none !important;
-        }
-        .x-gmail-data-detectors,
-        .x-gmail-data-detectors *,
-        .aBn {
-            border-bottom: 0 !important;
-            cursor: default !important;
-        }
-        .btn {
-            -webkit-transition: all 200ms ease;
-            transition: all 200ms ease;
-        }
-        .btn:hover {
-            background-color: dodgerblue;
-        }
-        @media screen and (max-width: 750px) {
-            .container {
-                width: 100%;
-                margin: auto;
-            }
-            .stack {
-                display: block;
-                width: 100%;
-                max-width: 100%;
-            }
-        }
-    </style>
+
+    <link href="{{ asset('css/mail.css') }}" rel="stylesheet">
 </head>
 
 <body>
