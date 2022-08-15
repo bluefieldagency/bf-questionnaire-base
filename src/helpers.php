9e5ce96e39f3fd794f62f2f3f966989b6a84b196
Erik <erik@holdorp.nl>
2022-08-15 13:33:00 +0200

    Helper function moved to package

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,17 @@
 <?php
 
-function repo_path($addPath = '') {
-    return base_path('vendor/immensenl/bf_questionnaire_base/src/' . ltrim($addPath, '/'));
+if ( ! function_exists('repo_path')) {
+    function repo_path($addPath = '')
+    {
+        return base_path('vendor/immensenl/bf_questionnaire_base/src/' . ltrim($addPath, '/'));
+    }
+}
+
+if ( ! function_exists('domain_route')) {
+    function domain_route($languageCode, $name, $parameters = [])
+    {
+        $domain = env('URL_' . strtoupper($languageCode));
+
+        return $domain . '/' . ltrim(route($name, $parameters, false), '/');
+    }
 }
