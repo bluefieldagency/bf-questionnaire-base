702f0623695ccc287a7a4ee5e9a2a54bf32dda4b
Erik Holdorp - Blue Field Agency <erik.holdorp@bluefieldagency.com>
2023-04-17 15:23:59 +0200

    Local mailspeed fixed

--- src/views/mail/layout.blade.php
+++ src/views/mail/layout.blade.php
@@ -8,14 +8,6 @@
     <![endif]-->
     <meta name="x-apple-disable-message-reformatting">
     <title></title>
-    <!--[if mso]>
-    <style>
-        * { font-family: Roboto, sans-serif !important; }
-    </style>
-    <![endif]-->
-    <!--[if !mso]><!-->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-    <!--<![endif]-->
 
     <link href="{{ asset('css/mail.css') }}" rel="stylesheet">
 </head>
